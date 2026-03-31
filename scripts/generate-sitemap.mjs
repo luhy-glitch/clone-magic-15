7754229bf8faa450b95cb6d6bc49199b762fcdb9
luhy-glitch <lucas.hytone@gmail.com>
2026-04-01 01:23:36 +0200

    SEO: Fully automated styled sitemap with 114 pages

--- scripts/generate-sitemap.mjs
+++ scripts/generate-sitemap.mjs
@@ -58,7 +58,7 @@ const urls = htmlFiles
     return `  <url>\n    <loc>${SITE_URL}${item.path}</loc>\n    <lastmod>${today}</lastmod>\n    <changefreq>monthly</changefreq>\n    <priority>${priority}</priority>\n  </url>`;
   });
 
-const xml = `<?xml version="1.0" encoding="UTF-8"?>\n<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n${urls.join("\n")}\n</urlset>\n`;
+const xml = `<?xml version="1.0" encoding="UTF-8"?>\n<?xml-stylesheet type="text/xsl" href="/sitemap.xsl"?>\n<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n${urls.join("\n")}\n</urlset>\n`;
 
 const destinations = [
   path.resolve(rootDir, "public/sitemap.xml"),
