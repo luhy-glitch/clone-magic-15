81e27639e8fae94a5ee4a11a559f36cba5ce8bc2
luhy-glitch <lucas.hytone@gmail.com>
2026-04-02 14:21:40 +0200

    Fix: Add missing FadeIn export to AnimatedSection to resolve build error

--- src/components/AnimatedSection.tsx
+++ src/components/AnimatedSection.tsx
@@ -7,6 +7,15 @@ export const AnimatedSection = forwardRef(({ children, className, id, ...props }
     </div>
   );
 });
-
 AnimatedSection.displayName = 'AnimatedSection';
+
+export const FadeIn = forwardRef(({ children, className, id, delay, ...props }: any, ref: any) => {
+  return (
+    <div ref={ref} className={className} id={id} {...props}>
+      {children}
+    </div>
+  );
+});
+FadeIn.displayName = 'FadeIn';
+
 export default AnimatedSection;
