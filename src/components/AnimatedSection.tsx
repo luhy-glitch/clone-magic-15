fc6e009cbb8d34fe9847bcf97689e4760c778e4e
luhy-glitch <lucas.hytone@gmail.com>
2026-04-02 14:24:16 +0200

    Fix: Export all required mock animation components (ScaleIn, SlideIn, etc) to ensure successful build

--- src/components/AnimatedSection.tsx
+++ src/components/AnimatedSection.tsx
@@ -1,21 +1,33 @@
 import React, { forwardRef } from 'react';
 
-export const AnimatedSection = forwardRef(({ children, className, id, ...props }: any, ref: any) => {
-  return (
-    <div ref={ref} className={className} id={id} {...props}>
-      {children}
-    </div>
-  );
-});
+// Huvudkomponenten
+export const AnimatedSection = forwardRef(({ children, className, id, ...props }: any, ref: any) => (
+  <div ref={ref} className={className} id={id} {...props}>{children}</div>
+));
 AnimatedSection.displayName = 'AnimatedSection';
 
-export const FadeIn = forwardRef(({ children, className, id, delay, ...props }: any, ref: any) => {
-  return (
-    <div ref={ref} className={className} id={id} {...props}>
-      {children}
-    </div>
-  );
-});
+// FadeIn
+export const FadeIn = forwardRef(({ children, className, id, delay, ...props }: any, ref: any) => (
+  <div ref={ref} className={className} id={id} {...props}>{children}</div>
+));
 FadeIn.displayName = 'FadeIn';
+
+// ScaleIn
+export const ScaleIn = forwardRef(({ children, className, id, delay, ...props }: any, ref: any) => (
+  <div ref={ref} className={className} id={id} {...props}>{children}</div>
+));
+ScaleIn.displayName = 'ScaleIn';
+
+// SlideIn (ifall den används någonstans)
+export const SlideIn = forwardRef(({ children, className, id, delay, direction, ...props }: any, ref: any) => (
+  <div ref={ref} className={className} id={id} {...props}>{children}</div>
+));
+SlideIn.displayName = 'SlideIn';
+
+// StaggerContainer (vanlig i liknande teman)
+export const StaggerContainer = forwardRef(({ children, className, id, ...props }: any, ref: any) => (
+  <div ref={ref} className={className} id={id} {...props}>{children}</div>
+));
+StaggerContainer.displayName = 'StaggerContainer';
 
 export default AnimatedSection;
